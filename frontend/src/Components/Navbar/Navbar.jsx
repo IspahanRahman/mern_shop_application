57208c35fa376ef78a18d5b2b407eda745a3884a
IspahanRahman <ispahanrahman133@gmail.com>
2024-02-11 08:17:52 +0600

    project setup and frontend initial start with navbar

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <img 
+          src={logo} 
+          alt='logo' 
+        />
+        <p>Shopper</p>
+      </div>
+      <ul className='nav-menu'>
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
