aa5ec8aca4a05a43bbcdb839f6d118438b6019ec
IspahanRahman <ispahanrahman133@gmail.com>
2024-03-04 14:36:58 +0600

    login and shop category page , shopContext implemented

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import './CSS/LoginSignup.css';
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className="loginSignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" className=""/>
+          <input type="email" placeholder="Your Name" className=""/>
+          <input type="password" placeholder="Enter Password"/>
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already Have and account? <span>Login Here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id=""/>
+          <p>By continuing, I agree to the terms and conditions</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
